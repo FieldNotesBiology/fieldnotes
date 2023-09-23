95e471cb9aaeb06017a4168d7c826973e41e9f51
rnadim9 <145862699+rnadim9@users.noreply.github.com>
2023-09-23 19:54:41 -0400

    Add files via upload

--- samplecode.py
+++ samplecode.py
@@ -1,70 +1,98 @@
 import streamlit as st
-import pymongo
-from pymongo import MongoClient
+import requests
+from datetime import datetime
 
-#rachels mongodb uri
-mongo_uri = "mongodb+srv://rnd22:<RaeRae13!>@cluster0.nmrig91.mongodb.net/?retryWrites=true&w=majority"
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://roxannanadim:7eWirSgMl45wOVpW@cluster1.6m028og.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Access your MongoDB database (replace 'your_database' with your actual database name)
+db = client.FieldNotes
+
+# Access your MongoDB collection (replace 'your_collection' with your actual collection name)
+collection = db.TestEntry
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 
-# Create a MongoDB client
-client = MongoClient(mongo_uri)
+# Function to fetch geolocation using Google Geolocation API
+def get_user_location():
+    try:
+        API_KEY = 'AIzaSyC6DrPvw4hcB-LrFGUAZ48uj83mJXkWeLc'
+        GEOLOCATION_API_URL = f'https://www.googleapis.com/geolocation/v1/geolocate?key={API_KEY}'
 
-# Access your database (replace 'mydatabase' with your actual database name)
-db = client["mydatabase"]
+        response = requests.post(GEOLOCATION_API_URL)
+        data = response.json()
+
+        if 'location' in data:
+            latitude = data['location']['lat']
+            longitude = data['location']['lng']
+            return latitude, longitude
+        else:
+            st.warning("Location not found.")
+            return None
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+        return None
+
 # Title and description
-
 st.title("Field Biologist Notebook")
 st.write("Add notes with geolocation data, photos, videos, and audio.")
 
-
 # Note input
 note_title = st.text_area("Note Title")
-note_text = st.text_area("Enter your note here")
-
-# JavaScript code to access the Geolocation API
-geolocation_code = """
-<script>
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
-
-    function showPosition(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        document.getElementById("location").innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
-    }
-
-    // Call the getLocation function when the page loads
-    getLocation();
-</script>
-"""
-
-# Display the HTML content with JavaScript code
-st.write(geolocation_code, unsafe_allow_html=True)
-
-# Display the location information
-st.markdown("<p id='location'>Location: Loading...</p>", unsafe_allow_html=True)
+note_text = st.text_area("Enter notes here")
 
 # Geolocation
 if st.checkbox("Include Geolocation"):
-    latitude = st.number_input("Latitude")
-    longitude = st.number_input("Longitude")
+    latitude, longitude = get_user_location()
+    if latitude is not None and longitude is not None:
+        st.write(f"Geolocation: Latitude: {latitude}, Longitude: {longitude}")
+    else:
+        st.write("Geolocation: Not Available")
 
-uploaded_file = None  # Initialize to None by default
-
+uploaded_file = None
 # Media Upload
 media_option = st.selectbox("Select Media Type", ["None", "Photo", "Video", "Audio"])
 if media_option != "None":
     uploaded_file = st.file_uploader(f"Upload {media_option}", type=["jpg", "png", "mp4", "wav"])
 
 # Save Button
+# Save the note data to your database or storage
 if st.button("Save Note"):
-    # Save the note data to your database or storage
-    st.success("Note saved successfully!")
+    if note_title and note_text:
+
+        # Generate a unique ID based on the current date and time
+        note_id = datetime.now().strftime("%Y%m%d%H%M%S")
+        
+        # Create a note doc from user input
+        note = {
+            "_id": note_id,  # Use the generated ID as the document's ID
+            "title": note_title,
+            "content": note_text,
+            "latitude": latitude,  # Separate key for latitude
+            "longitude": longitude,  # Separate key for longitude
+        }
+
+        # Insert into MongoDB
+        result = collection.insert_one(note)
+
+        if result.inserted_id:
+            st.success(f"Note saved with ID: {result.inserted_id}")
+        else:
+            st.error("Note could not be saved.")
+    else:
+        st.warning("Please provide a title and content for the note.")
+
 
 # Display uploaded media
 if uploaded_file:
@@ -74,4 +102,6 @@ if uploaded_file:
     elif media_option == "Video":
         st.video(uploaded_file)
     elif media_option == "Audio":
-        st.audio(uploaded_file)
+       st.audio(uploaded_file)
+
+
